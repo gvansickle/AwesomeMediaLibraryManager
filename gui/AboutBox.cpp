d406a0d17451a5a14adf0bb390ff971e08acdd54
Gary R. Van Sickle <grvs@users.sourceforge.net>
2017-12-03 22:28:06 -0600

    Adjustments.

--- gui/AboutBox.cpp
+++ gui/AboutBox.cpp
@@ -30,24 +30,26 @@ class AboutPage : public QWizardPage
 public:
     AboutPage(QWidget* parent) : QWizardPage(parent)
     {
-        setTitle(QString("About %1").arg(qApp->applicationDisplayName()));
-        setSubTitle(QString("The Awesome Media Library Manager"));
+        setTitle(QString("%1").arg(qApp->applicationDisplayName()));
+        setSubTitle(QString("<p>The Awesome Media Library Manager</p>"
+                            "<p>Version %1</p>"
+                            "<p>Copyright 2017 Gary R. Van Sickle (grvs@users.sourceforge.net).</p>").arg(qApp->applicationVersion()));
 
         m_label = new QLabel(this);
         m_label->setTextFormat(Qt::RichText);
         m_label->setText(tr("<body>"
-                             "<a href=\"https://github.com/gvansickle/AwesomeMediaLibraryManager\">AwesomeMediaLibraryManager</a> is free software: you can redistribute it and/or modify"
+                             "<p><a href=\"https://github.com/gvansickle/AwesomeMediaLibraryManager\">AwesomeMediaLibraryManager</a> is free software: you can redistribute it and/or modify"
                                 " it under the terms of the GNU General Public License as published by"
                                 " the Free Software Foundation, either version 3 of the License, or"
                                 " (at your option) any later version."
-                                "<br>"
-                                " AwesomeMediaLibraryManager is distributed in the hope that it will be useful,"
+                                "</p>"
+                                "<p>AwesomeMediaLibraryManager is distributed in the hope that it will be useful,"
                                 " but WITHOUT ANY WARRANTY; without even the implied warranty of"
                                 " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
                                 " GNU General Public License for more details."
-                                "<br>"
-                                "You should have received a copy of the GNU General Public License"
-                                " along with AwesomeMediaLibraryManager.  If not, see <a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>."
+                                "</p>"
+                                "<p>You should have received a copy of the GNU General Public License"
+                                " along with AwesomeMediaLibraryManager.  If not, see <a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>.</p>"
                             "</body>"));
         m_label->setWordWrap(true);
         m_label->setTextInteractionFlags(Qt::LinksAccessibleByKeyboard | Qt::LinksAccessibleByMouse);
@@ -65,7 +67,10 @@ private:
 
 AboutBox::AboutBox(QWidget *parent, const Qt::WindowFlags &flags) : QWizard(parent, flags)
 {
-    setOptions(QWizard::NoBackButtonOnStartPage | QWizard::NoBackButtonOnLastPage | QWizard::HaveFinishButtonOnEarlyPages);
+    setOptions(QWizard::NoBackButtonOnStartPage | QWizard::NoBackButtonOnLastPage | QWizard::HaveFinishButtonOnEarlyPages | QWizard::NoCancelButton);
+
+    // Turn the "Finish" button into an "Ok" button.
+    setButtonText(QWizard::FinishButton, tr("OK"));
 
     setWindowTitle(tr("About %1").arg(qApp->applicationDisplayName()));
 
