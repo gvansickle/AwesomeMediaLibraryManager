104308b17a359f9245f34fb15f206a9ef820a491
Gary R. Van Sickle <grvs@users.sourceforge.net>
2017-12-17 14:29:17 -0600

    Moew git version/dirty stamping.

--- cmake/GetVersionInfo.cmake
+++ cmake/GetVersionInfo.cmake
@@ -17,6 +17,12 @@
 # along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# Returns the following repo version info:
+# - GVI_GIT_DESCRIBE_OUTPUT: The raw output from "git describe"
+# - GVI_VERSION_TAG_QUAD: Four-segment version number from the last git tag, e.g. "1.2.3.4"
+# - GVI_VERSION_HASH: The git SHA-1 hash returned by "git describe".
+# - GVI_VERSION_IS_DIRTY: TRUE or FALSE, depending on what git describe returns for dirty.
+# - GVI_VERSION_DIRTY_POSTFIX: "-dirty" or "" depending on if the workspace is dirty or not.
 find_package(Git)
 if(GIT_FOUND)
     message(STATUS "Git found: ${GIT_EXECUTABLE}")
@@ -27,19 +33,19 @@ if(GIT_FOUND)
             OUTPUT_VARIABLE GVI_GIT_DESCRIBE_OUTPUT
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+" GVI_VERSION_TAG_QUAD ${GVI_GIT_DESCRIBE_OUTPUT})
-    string(REGEX REPLACE "-g([0-9a-fA-F]+)" "\\1" DUMMY ${GVI_GIT_DESCRIBE_OUTPUT})
-    set(GVI_VERSION_HASH ${CMAKE_MATCH_1})
-    if(${GVI_GIT_DESCRIBE_OUTPUT} MATCHES "-dirty")
-        set(GVI_VERSION_IS_DIRTY TRUE)
-        set(GVI_VERSION_DIRTY_POSTFIX "-dirty")
-    else()
-        set(GVI_VERSION_IS_DIRTY FALSE)
-        set(GVI_VERSION_DIRTY_POSTFIX "")
-    endif()
-    message(STATUS "git describe reports: ${GVI_GIT_DESCRIBE_OUTPUT} (${GVI_VERSION_TAG_QUAD}/${GVI_VERSION_HASH}/${GVI_VERSION_DIRTY_POSTFIX})")
-    set(GVI_PROJECT_VERSION "0.0.0.0")
 else()
     message(STATUS "Git not found")
-    set(GVI_PROJECT_VERSION "0.0.0.0")
+    set(GVI_GIT_DESCRIBE_OUTPUT "0.0.0.0-0-g0000000-dirty")
 endif()
+
+string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+" GVI_VERSION_TAG_QUAD ${GVI_GIT_DESCRIBE_OUTPUT})
+string(REGEX REPLACE "-g([0-9a-fA-F]+)" "\\1" DUMMY ${GVI_GIT_DESCRIBE_OUTPUT})
+set(GVI_VERSION_HASH ${CMAKE_MATCH_1})
+if(${GVI_GIT_DESCRIBE_OUTPUT} MATCHES "-dirty")
+    set(GVI_VERSION_IS_DIRTY TRUE)
+    set(GVI_VERSION_DIRTY_POSTFIX "-dirty")
+else()
+    set(GVI_VERSION_IS_DIRTY FALSE)
+    set(GVI_VERSION_DIRTY_POSTFIX "")
+endif()
+message(STATUS "git describe reports: ${GVI_GIT_DESCRIBE_OUTPUT} (${GVI_VERSION_TAG_QUAD}/${GVI_VERSION_HASH}/${GVI_VERSION_DIRTY_POSTFIX})")
