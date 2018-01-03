84e688957fdb51c3bc908e047be02ab9bf20d282
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-01-02 20:23:44 -0600

    Initial commit of Tips.cpp/.h.

---
+++ gui/helpers/Tips.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Tips.cpp
+ * Author: gary
+ * 
+ * Created on January 2, 2018, 8:23 PM
+ */
+
+#include "Tips.h"
+
+Tips::Tips()
+{
+}
+
+Tips::Tips(const Tips& orig)
+{
+}
+
+Tips::~Tips()
+{
+}
+
---
+++ gui/helpers/Tips.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Tips.h
+ * Author: gary
+ *
+ * Created on January 2, 2018, 8:23 PM
+ */
+
+#ifndef TIPS_H
+#    define TIPS_H
+
+class Tips
+{
+public:
+	Tips();
+	Tips(const Tips& orig);
+	virtual ~Tips();
+private:
+
+};
+
+#endif /* TIPS_H */
+
