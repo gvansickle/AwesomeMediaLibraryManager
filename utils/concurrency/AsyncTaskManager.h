f2fe99c94a431f2b443cb405ae8179346753ffbe
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-01-06 20:06:58 -0600

    Initial add.

---
+++ utils/concurrency/AsyncTaskManager.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_CONCURRENCY_ASYNCTASKMANAGER_H_
+#define UTILS_CONCURRENCY_ASYNCTASKMANAGER_H_
+
+#include <QObject>
+
+/*
+ *
+ */
+class AsyncTaskManager: public QObject
+{
+public:
+	AsyncTaskManager();
+	virtual ~AsyncTaskManager();
+};
+
+#endif /* UTILS_CONCURRENCY_ASYNCTASKMANAGER_H_ */
