14a775334580b4b581f182ba2dbc3895bce83679
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-02-10 18:29:52 -0600

    wait() is now working properly.

--- utils/concurrency/ExtFuture.h
+++ utils/concurrency/ExtFuture.h
@@ -59,23 +59,50 @@ class ExtFuture : public QFutureInterface<T>
 
 public:
 	/**
-	 * @brief ExtFuture
+	 * Default constructor.
 	 *
 	 * @note Regarding the initial state: From a comment in QtCreator's runextensions.h::AsyncJob constructor:
 	 * "we need to report it as started even though it isn't yet, because someone might
 	 * call waitForFinished on the future, which does _not_ block if the future is not started"
+	 * That code also does this:
+	 *
+	 * 		m_future_interface.setRunnable(this);
+	 *
+	 * Not sure if we need to do that here or not.
+	 *
+	 * This is the code we're fighting:
+	 *
+	 * void QFutureInterfaceBase::waitForFinished()
+		{
+			QMutexLocker lock(&d->m_mutex);
+			const bool alreadyFinished = !isRunning();
+			lock.unlock();
+
+			if (!alreadyFinished) {
+				d->pool()->d_func()->stealAndRunRunnable(d->runnable);
+
+				lock.relock();
+
+				while (isRunning())
+					d->waitCondition.wait(&d->m_mutex);
+			}
+
+			d->m_exceptionStore.throwPossibleException();
+		}
+	 *
 	 *
 	 * @param initialState
 	 */
-	ExtFuture(QFutureInterfaceBase::State initialState = QFutureInterfaceBase::Started) : QFutureInterface<T>(initialState)
+	ExtFuture(QFutureInterfaceBase::State initialState = QFutureInterfaceBase::State(QFutureInterfaceBase::State::Started | QFutureInterfaceBase::State::Running))
+		: QFutureInterface<T>(initialState)
 	{
-		qDebug() << M_THREADNAME() << "future state:" << state();
+		qDb() << "Passed state:" << initialState << "ExtFuture state:" << state();
 	}
 
 	ExtFuture(const QFutureInterface<T> &other)
 		: QFutureInterface<T>(other)
 	{
-		qDebug() << M_THREADNAME() << "future state:" << state();
+		qDb() << "future state:" << state();
 	}
 
 	/**
@@ -138,9 +165,9 @@ public:
 	 * @param other
 	 * @return
 	 */
-	bool operator==(const ExtFuture<T>& other) const;
+//	bool operator==(const ExtFuture<T>& other) const;
 
-	bool operator==(const QFuture<T>& other) const { return this->future() == other; }
+//	bool operator==(const QFuture<T>& other) const { return this->future() == other; }
 
 	/// @} // END Operators
 
@@ -162,10 +189,11 @@ T ExtFuture<T>::get()
 template<typename T>
 void ExtFuture<T>::wait()
 {
-	qDebug() << M_THREADNAME() << "WAIT START, future state:" << state();
-	//waitForFinished();
+	qDb() << "WAIT START, future state:" << state();
+	this->waitForFinished();
 	this->results();
-	qDebug() << M_THREADNAME() << "WAIT COMPLETE, future state:" << state();
+	this->cancel();
+	qDb() << "WAIT COMPLETE, future state:" << state();
 }
 
 template<typename T>
@@ -227,11 +255,11 @@ QString ExtFuture<T>::state() const
 	}
 }
 
-template<typename T>
-bool ExtFuture<T>::operator==(const ExtFuture<T>& other) const
-{
-	return QFutureInterface<T>::operator==(other);
-}
+//template<typename T>
+//bool ExtFuture<T>::operator==(const ExtFuture<T>& other) const
+//{
+//	return QFutureInterface<T>::operator==(other);
+//}
 
 #if 0
 template<typename T>
@@ -276,7 +304,7 @@ QDebug operator<<(QDebug dbg, const ExtFuture<T> &extfuture)
 {
 	QDebugStateSaver saver(dbg);
 
-	dbg << M_THREADNAME() << "ExtFuture<T>(" /*<< extfuture.future()*/ << "STATE:" << extfuture.state() << ")";
+	dbg << "ExtFuture<T>(" /*<< extfuture.future()*/ << "STATE:" << extfuture.state() << ")";
 
 	return dbg;
 }
