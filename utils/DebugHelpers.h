1210ac6f0ada3b172e0733c8cb01fd014006a289
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-02-11 17:42:25 -0600

    Added M_PRINT_TYPEOF_VAR_IN_ERROR().

--- utils/DebugHelpers.h
+++ utils/DebugHelpers.h
@@ -65,14 +65,34 @@ idstr(const char *id_as_c_str, T id)
 
 #define M_IDSTR(id) idstr(#id ": ", id) + ", " +
 
+/// Attempts to get the compiler to print a human-readable type at compile time.
+/// @note In the 21st century, this should be a solved problem.  It isn't.
+/// @see https://stackoverflow.com/a/46339450, https://stackoverflow.com/a/30276785
+//		typedef typename ft_test2_class::something_made_up X;
+//		bool x = decltype(&ft_test2_class::ft_test2_class)::nothing;
+template<typename T>
+void print_type_in_compilation_error(T&&)
+{
+	static_assert(std::is_same<T, int>::value && !std::is_same<T, int>::value, "Compilation failed because you wanted to read the type. See below");
+}
+/// Usage: use this anywhere to print a type name.
+/// Prints as a side-effect of failing the compile.
+/// @note Only works if t is a variable.
+//#define M_PRINT_TYPE_IN_ERROR(t) void dummy(void) { print_type_in_compilation_error((t)); }
+//#define M_PRINT_TYPE_IN_ERROR(T) typedef typename T::something_made_up X;
+#define M_PRINT_TYPEOF_VAR_IN_ERROR(v) bool x = decltype((v))::no_such_member_so_you_can_see_the_type_name;
 
-/// Portable compile-time warning.
-/// Use: M_WARNING("My message")
-///
+/// @name Preprocessor helpers for M_WARNING().
+/// @{
 #define STRINGISE_IMPL(x) #x
 #define STRINGISE(x) STRINGISE_IMPL(x)
 #define FILE_LINE_LINK __FILE__ "(" STRINGISE(__LINE__) "): "
+/// @}
 
+/**
+ * Portable compile-time warning message.
+ * Use: M_WARNING("My message")
+ */
 #if defined(_MSC_VER)
 #   define M_WARNING(exp) __pragma(message(FILE_LINE_LINK "warning C2660: " exp))
 #elif defined(__clang__)
