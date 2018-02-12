3f0144ec1f644d16ef5126975b1edbe5da416f77
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-02-11 21:22:11 -0600

    Added ExtAsyncTaskRunner.

--- utils/concurrency/ExtAsyncTask.h
+++ utils/concurrency/ExtAsyncTask.h
@@ -23,11 +23,11 @@
 /// Based on this Stack Overflow reply: https://stackoverflow.com/a/16729619
 
 /**
- * Subclass this to get a controllable task which can be passed to ReportingRunner::run().
+ * Subclass this to get a controllable, reporting-enabled task which can be passed to one of the ExtAsync::run() overloads.
  * Note that this class does not derive from QObject (or any other class for that matter).
- * @tparam T  The type returned in the QFuture<T>.
+ * @tparam T  The type returned in the ExtFuture<T>.
  */
-template <typename T, typename FutureTemplateType>
+template <typename T, typename FutureType = ExtFuture<T>>
 class ExtAsyncTask
 {
 public:
@@ -50,13 +50,106 @@ public:
 	 * you don't know a priori what the range max is (e.g. during a directory traversal).
 	 * If you do so, the GUI slows to a crawl.
 	 */
-	virtual void run(FutureTemplateType<T>& control) = 0;
+	virtual void run(FutureType& control) = 0;
 };
 
-template<class T, typename FutureTemplateType>
-ExtAsyncTask<T, FutureTemplateType>::~ExtAsyncTask()
+
+/**
+ * Used as an intermediary helper class between the ExtAsyc::run() functions and an
+ * instance of ExtAsyncTask<T>.  Handles the setup and teardown.  Setup includes reporting started and
+ * not starting if already cancelled.  Teardown includes reporting finished, if finishing was
+ * due to cancellation, and propagating and reporting exceptions.
+ *
+ * @note This is a sort of combination of the RunFunctionTaskBase<> and RunFunctionTask<> class templates
+ * from Qt5.
+ *
+ * @see /usr/include/qt5/QtConcurrent/qtconcurrentrunbase.h
+ */
+template <typename T>
+class ExtAsyncTaskRunner : public ExtFuture<T>, public QRunnable
+{
+public:
+    explicit ExtAsyncTaskRunner(ExtAsyncTask<T>* tsk) : m_task(tsk) { }
+    virtual ~ExtAsyncTaskRunner() { delete m_task; }
+
+	/**
+	 * start() function analogous to those in Qt5's RunFunctionTaskBase<>.
+	 */
+    ExtFuture<T> start(QThreadPool *pool = QThreadPool::globalInstance());
+
+	/**
+	 * The run() function is analogous to that in Qt5's RunFunctionTask<> class template.
+	 * It's an overridden virtual function inherited from QRunnable.
+	 */
+	void run() override;
+
+	ExtAsyncTask<T> *m_task;
+};
+
+
+//// ExtAsyncTask<T> Implementation below.
+
+template<class T, typename FutureType>
+ExtAsyncTask<T, FutureType>::~ExtAsyncTask()
 {
 	// Nothing.
 }
+
+//// ExtAsyncTaskRunner<T> Implementation below.
+
+template<typename T>
+ExtFuture<T> ExtAsyncTaskRunner<T>::start(QThreadPool* pool)
+{
+	this->setThreadPool(pool);
+	this->setRunnable(this);
+	// Report that we've started via the QFutureInterface.
+	this->reportStarted();
+	/// @note The original has: "this->future();" here.
+	/// Since ExtFuture<-QFutureInterface and the latter is a
+	/// QFuture "factory" of sorts, returning a copy of ourselves seems
+	/// correct here.
+	ExtFuture<T> extfuture = *this;
+	pool->start(this, /*m_priority*/ 0);
+	return extfuture;
+}
+
+template<typename T>
+void ExtAsyncTaskRunner<T>::run()
+{
+	if (this->isCanceled())
+	{
+		this->reportFinished();
+		return;
+	}
+#ifndef QT_NO_EXCEPTIONS
+	try {
+#endif
+		// Run the actual worker function.
+		/// In Qt5 QtConcurrent, this is done somewhat differently.
+		/// A "StoredFunctorCall0" struct (or one of dozens of variants) from
+		/// the header /usr/include/qt5/QtConcurrent/qtconcurrentstoredfunctioncall.h
+		/// come into play here, and effectively does something like this:
+		///     void runFunctor() override { this->result = function(); }
+		/// Since we want to be able to send up interim results and progress, we
+		/// need to do something else.
+		this->m_task->run(*this);
+#ifndef QT_NO_EXCEPTIONS
+	} catch (QException &e) {
+		ExtFuture<T>::reportException(e);
+	} catch (...) {
+		ExtFuture<T>::reportException(QUnhandledException());
+	}
+#endif
+	if (this->isCanceled())
+	{
+		// Report that we were canceled.
+		this->reportCanceled();
+	}
+	else
+	{
+		this->reportFinished();
+	}
+}
+
 
 #endif /* UTILS_CONCURRENCY_EXTASYNCTASK_H_ */
