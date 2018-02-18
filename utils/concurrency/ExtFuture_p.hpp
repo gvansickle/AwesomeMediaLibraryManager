56300ce6c3387b3d9b0123cf55ce5c875f754589
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-02-17 20:36:04 -0600

    Interim.

--- utils/concurrency/ExtFuture_p.hpp
+++ utils/concurrency/ExtFuture_p.hpp
@@ -1,12 +1,26 @@
 /*
- * ExtFuture_p.hpp
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
  *
- *  Created on: Feb 17, 2018
- *      Author: gary
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef UTILS_CONCURRENCY_EXTFUTURE_P_HPP_
 #define UTILS_CONCURRENCY_EXTFUTURE_P_HPP_
+
+
 
 
 /**
@@ -22,5 +36,19 @@ ExtFuture<T>::unwrap()
 		;});
 }
 
+/**
+ * This is the function which actually is called by QtConcurrent::run() for the continuation.
+ */
+template<class T>
+static QString ThenHelper(ExtFuture<T>* predecessor_future)
+{
+	qDb() << "THEN CALLED, WAITING";
+	predecessor_future->wait();
+	qDb() << "THEN CALLED, WAIT OVER, CALLING CALLBACK";
+	Q_CHECK_PTR(predecessor_future);
+	Q_CHECK_PTR(predecessor_future->m_continuation_function);
+	(*(predecessor_future->m_continuation_function))();
+	return QString("THEN DONE");
+}
 
 #endif /* UTILS_CONCURRENCY_EXTFUTURE_P_HPP_ */
