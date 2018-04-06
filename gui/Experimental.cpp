ec508511a31f29f3d51d67804e20b0347c6172aa
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-04-05 20:20:55 -0500

    Minimally working gtkmm Gtk::FileChooser.

--- gui/Experimental.cpp
+++ gui/Experimental.cpp
@@ -27,8 +27,8 @@
 //#include <KUrlRequesterDialog>
 //#include <KBuildSycocaProgressDialog>
 
-//#include <gtkmm.h>
-//#include <gtkmm/filechooserdialog.h>
+#include <gtkmm.h>
+#include <gtkmm/filechooserdialog.h>
 #endif
 
 #if EX2 == 1
@@ -37,6 +37,9 @@
 #endif
 
 #include <QDebug>
+#include <utils/DebugHelpers.h>
+
+#include <QApplication>
 #include <QFileDialog>
 
 Experimental::Experimental(QWidget *parent) : QWidget(parent)
@@ -64,7 +67,7 @@ void Experimental::DoExperiment()
 
 //	QUrl url = KDirSelectDialog::selectDirectory(QUrl("file://home/gary"), false, this, "KDE4 Dir Select Dialog");
 
-#if 1
+#if 0
 	QUrl filePath = QFileDialog::getExistingDirectoryUrl(this, tr("Test - SHOULD BE NATIVE - getExistingDirectoryUrl()"),
 															QUrl("/home/gary"), // Start dir
 															QFileDialog::ShowDirsOnly, // Options.
@@ -90,18 +93,52 @@ void Experimental::DoExperiment()
 //	KUrlRequesterDialog::getUrl();
 
 /// GTKMM
-#if 0
+#if 1
 //	int argc = 1;
 //	char *argv[] = {"myapp", 0};
-	auto kit = Gtk::Application::create("myapp");
+
+	// Per https://developer.gnome.org/gtkmm/stable/classGtk_1_1Main.html#details
+	// "Deprecated:	Use Gtk::Application instead."
+	///Gtk::Main kit;
+
+	// Per https://developer.gnome.org/gtkmm/stable/classGtk_1_1Application.html#details
+	// "the application ID must be valid. See g_application_id_is_valid()."
+	// https://developer.gnome.org/gio/stable/GApplication.html#g-application-id-is-valid
+	// GIO::ApplicationFlags: https://developer.gnome.org/gio/stable/GApplication.html#GApplicationFlags
+	//    Not clear that any of the flags are applicable.
+	auto app = Gtk::Application::create(tostdstr(QApplication::desktopFileName()).c_str());
+	std::string chosen_path;
 
 	Gtk::FileChooserDialog dialog("TEST - GTK FILECHOOSER", Gtk::FileChooserAction::FILE_CHOOSER_ACTION_SELECT_FOLDER);
+	//dialog.set_transient_for(kit);
 	dialog.set_local_only(false);
 	dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
 	dialog.add_button("_Open", Gtk::RESPONSE_OK);
-	kit->run(dialog);
+	int result = dialog.run();
+//	int result = app->run(dialog);
+
+	switch(result)
+	{
+	case Gtk::RESPONSE_OK:
+	{
+		chosen_path = dialog.get_filename();
+		qDebug() << "Diretory selected:" << chosen_path;
+		break;
+	}
+	case Gtk::RESPONSE_CANCEL:
+	{
+		qDebug() << "User cancelled";
+		break;
+	}
+	default:
+	{
+		qDebug() << "Unknown result:" << result;
+		break;
+	}
+	}
 
 #endif
+
 #endif
 
 #if EX2 == 1
