725ec054bbf6158320ef120dc012084654180692
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-05-01 11:47:22 -0500

    Copyright notices.

--- src/gui/widgets/ActivityProgressPopup.h
+++ src/gui/widgets/ActivityProgressPopup.h
@@ -1,15 +1,27 @@
 /*
- * ActivityProgressPopup.h
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
  *
- *  Created on: Apr 30, 2018
- *      Author: gary
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SRC_GUI_WIDGETS_ACTIVITYPROGRESSPOPUP_H_
 #define SRC_GUI_WIDGETS_ACTIVITYPROGRESSPOPUP_H_
 
 /// Qt5
-
+class QWidget;
 #include <QWidgetAction>
 
 /// KF5
