0d4a4b38c598d82af1024d1b31eb3903425d7366
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-06-17 01:22:54 -0500

    Initial add SupportedMimeTypes.

---
+++ src/logic/SupportedMimeTypes.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_LOGIC_SUPPORTEDMIMETYPES_H_
+#define SRC_LOGIC_SUPPORTEDMIMETYPES_H_
+
+#include <QObject>
+#include <QVector>
+#include <QMimeType>
+
+/*
+ *
+ */
+class SupportedMimeTypes: public QObject
+{
+public:
+	SupportedMimeTypes();
+    ~SupportedMimeTypes() override;
+
+    QVector<QMimeType> supportedAudioMimeTypes() const;
+    QStringList supportedAudioMimeTypesAsExtensions() const;
+
+protected:
+    QStringList m_mime_audio_types;
+    QStringList m_mime_audio_associated_types;
+    QStringList m_mime_playlist_types;
+};
+
+#endif /* SRC_LOGIC_SUPPORTEDMIMETYPES_H_ */
