61ee0d4f7320888330551a0f5d5b1f57a1f5817c
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-08-25 18:52:07 -0500

    Initial add.

---
+++ src/kde_gdb_wrapper.sh
@@ -0,0 +1,9 @@
+# Depends on cwd being build-kdev/install/bin
+notify-send "PWD: $(pwd)";
+notify-send "KDEV_BUILD_INST_DIR_FOR_RUN_DEBUG: ${KDEV_BUILD_INST_DIR_FOR_RUN_DEBUG}";
+#source ../../prefix.sh
+
+# We're invoked with this, and it seems to work.
+### Or maybe this:
+### /home/gary/src/AwesomeMediaLibraryManager/src/kde_gdb_wrapper.sh gdb /home/gary/src/AwesomeMediaLibraryManager/src/kde_gdb_wrapper.sh gdb --interpreter=mi2 -quiet
+gdb --interpreter=mi2
