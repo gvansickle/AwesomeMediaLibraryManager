4e84a1daedd21254dc51c72f0d2cca79eaafc2e5
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-10-29 17:18:17 -0500

    Initial add, AbstractTreeModelHeaderItem.

---
+++ src/logic/models/AbstractTreeModelHeaderItem.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file AbstractTreeModelHeaderItem.h
+ */
+#ifndef SRC_LOGIC_MODELS_ABSTRACTTREEMODELHEADERITEM_H_
+#define SRC_LOGIC_MODELS_ABSTRACTTREEMODELHEADERITEM_H_
+
+#include "AbstractTreeModelItem.h"
+
+/*
+ *
+ */
+class AbstractTreeModelHeaderItem: public AbstractTreeModelItem
+{
+public:
+	AbstractTreeModelHeaderItem();
+	virtual ~AbstractTreeModelHeaderItem();
+};
+
+#endif /* SRC_LOGIC_MODELS_ABSTRACTTREEMODELHEADERITEM_H_ */
