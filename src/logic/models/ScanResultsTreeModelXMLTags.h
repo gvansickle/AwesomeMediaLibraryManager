6482da62e22aac6b2931c231a5a75cb8ef16d8c6
Gary R. Van Sickle <grvs@users.sourceforge.net>
2018-11-26 11:28:08 -0600

    Copyright notice.

--- src/logic/models/ScanResultsTreeModelXMLTags.h
+++ src/logic/models/ScanResultsTreeModelXMLTags.h
@@ -1,6 +1,21 @@
-//
-// Created by gary on 11/26/18.
-//
+/*
+ * Copyright 2018 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef AWESOMEMEDIALIBRARYMANAGER_SCANRESULTSTREEMODELXMLTAGS_H
 #define AWESOMEMEDIALIBRARYMANAGER_SCANRESULTSTREEMODELXMLTAGS_H
@@ -12,11 +27,6 @@
 // Qt5
 #include <QObject>
 #include <QtXml>
-
-class ScanResultsTreeModelXMLTags
-{
-
-};
 
 class DSRTag
 {
@@ -31,9 +41,9 @@ public:
 	Q_ENUM(TagName)
 
 	/**
-	 * Forwards DSRTag [] operator to the .at() function, so we don't populate the map but rather throw.
+	 * Forwards DSRTag operator[] to the std::map's .at() function, so we don't populate the map but rather throw.
 	 */
-	const std::string operator[](DSRTag::TagName i) { return m_dsrtag_to_string.at(i); };
+	const std::string operator[](DSRTag::TagName i) const { return m_dsrtag_to_string.at(i); };
 
 protected:
 	static std::map<DSRTag::TagName, std::string> m_dsrtag_to_string;
