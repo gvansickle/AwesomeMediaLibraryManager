1ce3ba47fc8987cab4a18110ddc0066c077baa68
Gary R. Van Sickle <grvs@users.sourceforge.net>
2019-05-31 20:39:05 -0500

    Initial add of UUIncD.

---
+++ src/logic/UUIncD.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "UUIncD.h"
+
+std::atomic_uint64_t UUIncD::m_next_id {0};
+
+UUIncD::UUIncD(std::uint64_t id)
+{
+	m_my_id = id;
+}
+
+// static
+UUIncD UUIncD::create()
+{
+	return UUIncD::m_next_id.fetch_add(1);
+}
---
+++ src/logic/UUIncD.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AWESOMEMEDIALIBRARYMANAGER_UUINCD_H
+#define AWESOMEMEDIALIBRARYMANAGER_UUINCD_H
+
+// Std C++
+#include <atomic>
+#include <cstdint>
+
+// Ours
+#include <future/guideline_helpers.h>
+
+class UUIncD
+{
+public:
+	M_GH_RULE_OF_FIVE_DEFAULT_C21(UUIncD);
+	~UUIncD();
+
+	static UUIncD create();
+
+protected:
+	UUIncD(std::uint64_t id);
+
+private:
+
+	std::uint64_t m_my_id;
+
+	/**
+	 * The program-global threadsafe 64-bit ID which will next be doled out by create().
+	 */
+	static std::atomic_uint64_t m_next_id;
+};
+
+
+#endif //AWESOMEMEDIALIBRARYMANAGER_UUINCD_H
