70496755e2d70ae14286fa0f7cfe274270b4f4b8
Gary R. Van Sickle <grvs@sourceforge.net>
2025-06-12 21:45:48 -0500

    New test suite, CueSheetTests{.h,.cpp}.

---
+++ tests/CueSheetTests.cpp
@@ -0,0 +1,37 @@
+/*
+* Copyright 2025 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of AwesomeMediaLibraryManager.
+ *
+ * AwesomeMediaLibraryManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AwesomeMediaLibraryManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeMediaLibraryManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ */
+
+#include "CueSheetTests.h"
+
+#include "CueSheet.h"
+
+TEST_F(CueSheetTests, CueSheetAssignment)
+{
+    CueSheet cs1, cs2;
+
+	cs1.set_origin(CueSheet::Origin::Embedded);
+
+	cs2 = cs1;
+
+	EXPECT_EQ(cs1, cs2);
+}
